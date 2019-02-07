3801df9773a9ff4b95d5244d332eac2bc07a79c5
hendisantika <hendisantika@yahoo.co.id>
2019-02-08 06:06:26 +0700

    #4 - Add contacts model

--- contacts/models.py
+++ contacts/models.py
@@ -1,1 +1,9 @@
 # Create your models here.
+from django.contrib.gis.db import models
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=50)
