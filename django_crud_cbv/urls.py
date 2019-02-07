81ee67a7cd503884bbc88c6856b3079d2dee7e1a
hendisantika <hendisantika@yahoo.co.id>
2019-02-08 06:40:44 +0700

    #19 - Modify urls.py file

--- django_crud_cbv/urls.py
+++ django_crud_cbv/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # [...]
+    path('contacts', views.ContactList.as_view(), name='contact_list'),
+    path('contact/<int:pk>', views.ContactDetail.as_view(), name='contact_detail'),
+    path('create', views.ContactCreate.as_view(), name='contact_create'),
+    path('update/<int:pk>', views.ContactUpdate.as_view(), name='contact_update'),
+    path('delete/<int:pk>', views.ContactDelete.as_view(), name='contact_delete'),
 ]
